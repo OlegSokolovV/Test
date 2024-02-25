6e908c7433992ec02eeee7b3f0e0e97032cbf330
OlegSokolovV <159052477+OlegSokolovV@users.noreply.github.com>
2024-02-25 17:52:33 +0200

    @define cout std::cout

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
+#define cout std::cout
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
-	std::cout << "TESTING!";
+	cout << "Hello World!" << std::endl;
+	cout << "TESTING!";
 
 	return 0;
 }
